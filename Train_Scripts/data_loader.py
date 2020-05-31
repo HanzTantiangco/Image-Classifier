7f64b63c22ccb5c17cb7b7f92110275ec3fe18b3
Hanz Tantiangco <hanz.tantiangco@hotmail.co.uk>
2020-05-31 12:05:51 +0100

    docs: Add docstrings in data_loader.py

--- Train_Scripts/data_loader.py
+++ Train_Scripts/data_loader.py
@@ -6,11 +6,30 @@ import json
 import torch.nn.functional as F
 
 def data_loader(directory):
+    """
+    Load and normalise images using torchvision.
+
+    Args:
+    directory: str. The file directory of the images from get_input_args.
+
+    Returns:
+    train_data
+    validation_data
+    test_data
+    train_loader
+    validation_loader
+    test_loader
+    cat_to_name: Dictionary mapping the encoded file categories to the actual
+    names of the images.
+
+    """
+
+
     data_dir = str(directory)
     train_dir = data_dir + '/train'
     valid_dir = data_dir + '/valid'
     test_dir = data_dir + '/test'
-    
+
     # Define transforms for the training, validation, and testing sets
     train_transforms = transforms.Compose([transforms.RandomRotation(30),
                                            transforms.RandomResizedCrop(224),
@@ -40,18 +59,19 @@ def data_loader(directory):
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
     validation_loader = torch.utils.data.DataLoader(validation_data, batch_size=32)
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=32)
-    
+
     # Test image shape
     dataiter = iter(train_loader)
     images, labels = dataiter.next()
     print(type(images))
     print(images.shape)
     print(labels.shape)
-    
+
     # Label mapping
     with open('cat_to_name.json', 'r') as f:
         cat_to_name = json.load(f)
         print(len(cat_to_name))
-    
-    
-    return train_data, validation_data, test_data, train_loader, validation_loader, test_loader, cat_to_name
+
+
+    return train_data, validation_data, test_data, train_loader,
+    validation_loader, test_loader, cat_to_name
