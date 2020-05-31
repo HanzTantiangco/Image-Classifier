f06e3cf666621a66881a9710088cc0b5f56d5a88
Hanz Tantiangco <hanz.tantiangco@hotmail.co.uk>
2020-05-31 14:31:11 +0100

    refactor: Remove random image selection code from predict/get_input_args.py

--- Predict_Scripts/get_input_args.py
+++ Predict_Scripts/get_input_args.py
@@ -1,15 +1,5 @@
 import argparse
 
-# Generates a random image to analyse
-import random, os
-
-folder_path = '/home/workspace/ImageClassifier/flowers/test/'
-folder_number = 0
-folder_number = random.randint(1,102)
-folder_path = folder_path + str(folder_number)
-random_select = random.choice(os.listdir(folder_path))
-random_image_path = folder_path + '/' + random_select
-
 def get_input_args():
     """
     This function takes a user input and returns a data structure which stores
@@ -20,7 +10,7 @@ def get_input_args():
 
     parser.add_argument('--gpu', type = bool, default=True, help =
     'Choose GPU for training')
-    parser.add_argument('--save_dir', type = str, default = random_image_path,
+    parser.add_argument('--save_dir', type = str,
     help = 'Set directory for image.')
     parser.add_argument('--checkpoint', type = str,
     default = '/home/workspace/ImageClassifier/checkpoint.pth',
